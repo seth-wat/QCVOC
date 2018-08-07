793b7b92fcbd00d33daa305fd13957a9e7fd9c7e
JP Dillingham <jpdillingham@gmail.com>
2018-08-06 21:57:30 -0500

    add optional IDbTransaction parameter to ICompositeRepository

--- api/QCVOC.Api/Data/Repository/Composite/ICompositeRepository{T}.cs
+++ api/QCVOC.Api/Data/Repository/Composite/ICompositeRepository{T}.cs
@@ -1,9 +1,15 @@
-﻿namespace QCVOC.Server.Data.Repository.Composite
+﻿using System.Data;
+
+namespace QCVOC.Server.Data.Repository.Composite
 {
     public interface ICompositeRepository<T>
     {
         T Add(T composite);
 
+        T Add(T composite, IDbTransaction transaction);
+
         void Remove(T composite);
+
+        void Remove(T composite, IDbTransaction transaction);
     }
 }
