fa9c016e9d3ed8177c8d4f7bb33033fd1a7a9505
JP Dillingham <jpdillingham@gmail.com>
2018-08-06 21:36:34 -0500

    Add composite types for modeling many to many relationships

---
+++ api/QCVOC.Api/Data/Model/Composite/EventsAccounts.cs
@@ -0,0 +1,10 @@
+﻿namespace QCVOC.Server.Data.Model.Composite
+{
+    using System;
+
+    public class EventsAccounts
+    {
+        public Guid AccountId { get; set; }
+        public Guid EventId { get; set; }
+    }
+}
---
+++ api/QCVOC.Api/Data/Model/Composite/EventsServices.cs
@@ -0,0 +1,10 @@
+﻿namespace QCVOC.Server.Data.Model.Composite
+{
+    using System;
+
+    public class EventsServices
+    {
+        public Guid EventId { get; set; }
+        public Guid ServiceId { get; set; }
+    }
+}
