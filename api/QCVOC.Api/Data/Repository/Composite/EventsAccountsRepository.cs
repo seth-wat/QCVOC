9625768fae1e666a30b10a3a9b2ff44a270f2bce
JP Dillingham <jpdillingham@gmail.com>
2018-08-06 21:37:08 -0500

    stub EventsAccountsRepository

---
+++ api/QCVOC.Api/Data/Repository/Composite/EventsAccountsRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace QCVOC.Server.Data.Repository.Composite
+{
+    using System;
+    using QCVOC.Server.Data.Model.Composite;
+
+    public class EventsAccountsRepository : ICompositeRepository<EventsAccounts>
+    {
+        public EventsAccounts Add(EventsAccounts composite)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(EventsAccounts composite)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
