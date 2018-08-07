c2ab110258ee447df7421f03101e6e933def03ef
JP Dillingham <jpdillingham@gmail.com>
2018-08-06 21:45:31 -0500

    add constructors to composite repositories

--- api/QCVOC.Api/Data/Repository/Composite/EventsAccountsRepository.cs
+++ api/QCVOC.Api/Data/Repository/Composite/EventsAccountsRepository.cs
@@ -1,10 +1,18 @@
 ﻿namespace QCVOC.Server.Data.Repository.Composite
 {
     using System;
+    using QCVOC.Api.Data.ConnectionFactory;
     using QCVOC.Server.Data.Model.Composite;
 
     public class EventsAccountsRepository : ICompositeRepository<EventsAccounts>
     {
+        public EventsAccountsRepository(IDbConnectionFactory dbConnectionFactory)
+        {
+            DbConnectionFactory = dbConnectionFactory;
+        }
+
+        private IDbConnectionFactory DbConnectionFactory { get; }
+
         public EventsAccounts Add(EventsAccounts composite)
         {
             throw new NotImplementedException();
--- api/QCVOC.Api/Data/Repository/Composite/EventsServicesRepository.cs
+++ api/QCVOC.Api/Data/Repository/Composite/EventsServicesRepository.cs
@@ -1,10 +1,18 @@
 ﻿namespace QCVOC.Server.Data.Repository.Composite
 {
     using System;
+    using QCVOC.Api.Data.ConnectionFactory;
     using QCVOC.Server.Data.Model.Composite;
 
     public class EventsServicesRepository : ICompositeRepository<EventsServices>
     {
+        public EventsServicesRepository(IDbConnectionFactory dbConnectionFactory)
+        {
+            DbConnectionFactory = dbConnectionFactory;
+        }
+
+        private IDbConnectionFactory DbConnectionFactory { get; }
+
         public EventsServices Add(EventsServices composite)
         {
             throw new NotImplementedException();
